14af12f2cbd7ac782ad9a119b53c5bfc698d46e6
Thet Naing Tun <thetnaingtun@bindez.com>
2022-06-03 10:00:05 +0700

    mongo db setup

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/thetnaingtn/go-dermacare-service/package/storage/mongo"
+)
+
+func main() {
+	mongo.Setup()
+}
---
+++ package/storage/mongo/db.go
@@ -0,0 +1,31 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+const URI = "mongodb://localhost:27017/dermacare"
+
+func Setup() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(URI))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+}
