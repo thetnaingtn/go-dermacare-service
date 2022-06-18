48a8a6c172611e9d485c86b4b0be643656e2216d
Thet Naing Tun <thetnaingtun@bindez.com>
2022-06-18 21:03:09 +0700

    add minimun stock to product

--- pkg/listing/product.go
+++ pkg/listing/product.go
@@ -15,6 +15,7 @@ type Product struct {
 	Categories   []primitive.ObjectID `json:"categories,omitempty" bson:"categories,omitempty"`
 	Description  string               `json:"description,omitempty" bson:"description,omitempty"`
 	Supplier     primitive.ObjectID   `json:"supplier,omitempty" bson:"supplier,omitempty"`
+	MinimumStock int                  `json:"miminum_quantity,omitempty" bson:"minimum_quantity,omitempty"`
 	ExpiredDate  time.Time            `json:"expired_date,omitempty" bson:"expired_date,omitempty"`
 	CreatedAt    time.Time            `json:"created_at,omitempty" bson:"created_at,omitempty"`
 	UpdatedAt    time.Time            `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
