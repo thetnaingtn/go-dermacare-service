2336ba176f4bafd0924647f4e5bdd4d2d4ebcb0d
Thet Naing Tun <thetnaingtun@bindez.com>
2022-06-18 08:48:27 +0700

    add more that one categories to products

--- pkg/adding/product.go
+++ pkg/adding/product.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Product struct {
-	Name         string             `json:"name" bson:"name"`
-	Quantity     int                `json:"quantity" bson:"quantity"`
-	Price        int64              `json:"price" bson:"price"`
-	SellingPrice int64              `json:"selling_price" bson:"selling_price"`
-	Category     primitive.ObjectID `json:"category,omitempty" bson:"category,omitempty"`
-	Description  string             `json:"description,omitempty" bson:"description,omitempty"`
-	Supplier     primitive.ObjectID `json:"supplier,omitempty" bson:"supplier,omitempty"`
-	ExpiredDate  time.Time          `json:"expired_date,omitempty" bson:"expired_date,omitempty"`
-	CreatedAt    time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UpdatedAt    time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	Name         string               `json:"name" bson:"name"`
+	Quantity     int                  `json:"quantity" bson:"quantity"`
+	Price        int64                `json:"price" bson:"price"`
+	SellingPrice int64                `json:"selling_price" bson:"selling_price"`
+	Categories   []primitive.ObjectID `json:"categories,omitempty" bson:"categories,omitempty"`
+	Description  string               `json:"description,omitempty" bson:"description,omitempty"`
+	Supplier     primitive.ObjectID   `json:"supplier,omitempty" bson:"supplier,omitempty"`
+	ExpiredDate  time.Time            `json:"expired_date,omitempty" bson:"expired_date,omitempty"`
+	CreatedAt    time.Time            `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt    time.Time            `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
