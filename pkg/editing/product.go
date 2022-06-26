7999d3be7d88e0d57013c794d61908314551b766
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2022-06-26 10:23:37 +0700

    fix: zero value update

--- pkg/editing/product.go
+++ pkg/editing/product.go
@@ -6,16 +6,17 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// need to add omitempty for bson. Otherwise, all the empty form payload are encoded into zero value(false, 0, "") and will update the db with those zero value.
 type ProductEditForm struct {
-	Name         string               `json:"name" bson:"name"`
-	Quantity     int                  `json:"quantity" bson:"quantity"`
-	Price        int64                `json:"price" bson:"price"`
-	SellingPrice int64                `json:"selling_price" bson:"selling_price"`
-	Categories   []primitive.ObjectID `json:"categories,omitempty" bson:"categories,omitempty"`
-	Description  string               `json:"description,omitempty" bson:"description,omitempty"`
-	Supplier     primitive.ObjectID   `json:"supplier,omitempty" bson:"supplier,omitempty"`
-	MinimumStock int                  `json:"miminum_stock,omitempty" bson:"minimum_stock,omitempty"`
-	ExpiredDate  time.Time            `json:"expired_date,omitempty" bson:"expired_date,omitempty"`
-	CreatedAt    time.Time            `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UpdatedAt    time.Time            `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	Name         string               `json:"name" bson:"name,omitempty"`
+	Quantity     int                  `json:"quantity" bson:"quantity,omitempty"`
+	Price        int64                `json:"price" bson:"price,omitempty"`
+	SellingPrice int64                `json:"selling_price" bson:"selling_price,omitempty"`
+	Categories   []primitive.ObjectID `json:"categories" bson:"categories,omitempty"`
+	Description  string               `json:"description" bson:"description,omitempty"`
+	Supplier     primitive.ObjectID   `json:"supplier" bson:"supplier,omitempty"`
+	MinimumStock int                  `json:"miminum_stock" bson:"minimum_stock,omitempty"`
+	ExpiredDate  time.Time            `json:"expired_date" bson:"expired_date,omitempty"`
+	CreatedAt    time.Time            `json:"created_at" bson:"created_at,omitempty"`
+	UpdatedAt    time.Time            `json:"updated_at" bson:"updated_at,omitempty"`
 }
