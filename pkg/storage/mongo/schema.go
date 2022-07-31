099f1702efb1f0ffd7ad0505ae08339df1c576e2
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2022-07-31 12:04:43 +0700

    add schema validation for products

---
+++ pkg/storage/mongo/schema.go
@@ -0,0 +1,43 @@
+package mongo
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var productSchema = bson.M{
+	"bsonType": "object",
+	"required": []string{"name", "quantity", "price", "selling_price"},
+	"properties": bson.M{
+		"name": bson.M{
+			"bsonType":    "string",
+			"description": "product name is required",
+		},
+		"quantity": bson.M{
+			"bsonType":    "int",
+			"description": "quantity is required",
+		},
+		"price": bson.M{
+			"bsonType":    "long",
+			"description": "price is required",
+		},
+		"selling_price": bson.M{
+			"bsonType":    "long",
+			"description": "selling price is required",
+		},
+		"minimum_stock": bson.M{
+			"bsonType":    "int",
+			"description": "minimum stock is required",
+		},
+	},
+}
+
+func getSchemaValidation(name string) *options.CreateCollectionOptions {
+	var schema primitive.M
+	if name == "products" {
+		schema = productSchema
+	}
+
+	return options.CreateCollection().SetValidator(schema)
+}
