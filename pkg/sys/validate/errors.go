cf7cd8f1ace70e2d3d7f43723954db87c817bca8
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2022-10-24 13:40:33 +0700

    add field validation errors

--- pkg/sys/validate/errors.go
+++ pkg/sys/validate/errors.go
@@ -1,6 +1,11 @@
 package validate
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type RequestError struct {
 	Err    error
@@ -21,4 +26,47 @@ func (re *RequestError) Error() string {
 func IsRequestError(err error) bool {
 	var re *RequestError
 	return errors.As(err, &re)
+}
+
+type FieldError struct {
+	Error string `json:"error"`
+	Field string `json:"field"`
+}
+
+type FieldErrors []FieldError
+
+func (fe FieldErrors) Error() string {
+	fieldErrors, err := json.Marshal(fe)
+	if err != nil {
+		return err.Error()
+	}
+
+	return string(fieldErrors)
+}
+
+func IsFieldErrors(err error) bool {
+	var fe FieldErrors
+	return errors.As(err, &fe)
+}
+
+func GetFieldsValidationErrors(e error) FieldErrors {
+	var ve validator.ValidationErrors
+	if ok := errors.As(e, &ve); ok {
+		errs := make([]FieldError, len(ve))
+		for i, e := range ve {
+			errorMsg := FieldError{Field: e.Field(), Error: msgForTag(e.Tag())}
+			errs[i] = errorMsg
+		}
+		return errs
+	}
+	return nil
+}
+
+func msgForTag(tag string) string {
+	switch tag {
+	case "required":
+		return "This field is required"
+	default:
+		return "Invalid tag"
+	}
 }
