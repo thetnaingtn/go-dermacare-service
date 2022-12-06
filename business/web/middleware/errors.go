2d9ee02b41953bffa7c2237f4ab0e070120af08f
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2022-12-06 21:28:24 +0700

    add error middleware

---
+++ business/web/middleware/errors.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thetnaingtn/go-dermacare-service/pkg/sys/validate"
+)
+
+func Error() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Next()
+		for _, errMsg := range ctx.Errors {
+			unWrapErr := errMsg.Unwrap()
+			var re validate.ErrorResponse
+			var status int
+			switch err := unWrapErr.(type) {
+			case *validate.RequestError:
+				re = validate.ErrorResponse{
+					Error: err.Error(),
+				}
+				status = err.Status
+			case validate.FieldErrors:
+				re = validate.ErrorResponse{
+					Error:  "validation errors",
+					Fields: err.Fields(),
+				}
+				status = http.StatusBadRequest
+			default:
+				re = validate.ErrorResponse{
+					Error: "server error",
+				}
+				status = http.StatusInternalServerError
+			}
+			ctx.JSON(status, re)
+		}
+		ctx.Abort()
+	}
+}
