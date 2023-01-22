fa105ef7894413cc0a3df464af7244bc24cc72d2
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-01-22 10:03:46 +0700

    add update,delete unit test for category

--- business/data/store/category/category_test.go
+++ business/data/store/category/category_test.go
@@ -1,11 +1,13 @@
 package category_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/thetnaingtn/go-dermacare-service/business/data/store/category"
 	"github.com/thetnaingtn/go-dermacare-service/business/data/test"
+	"github.com/thetnaingtn/go-dermacare-service/business/sys/validate"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -43,9 +45,38 @@ func TestCategory(t *testing.T) {
 			t.Logf("\t%s\tTest %d:\tShould be able to retrieve category by ID.", test.Success, testID)
 
 			if diff := cmp.Diff(c, saved); diff != "" {
-				t.Fatalf("\t%s\tTest %d:\tShould get back the same category. Diff:\n%s", test.Failed, testID, diff)
+				t.Errorf("\t%s\tTest %d:\tShould get back the same category. Diff:\n%s", test.Failed, testID, diff) // use t.Errorf until can figure out how to deal with time
 			}
 			t.Logf("\t%s\tTest %d:\tShould get back the same category.", test.Success, testID)
+
+			uc := category.UpdateCategory{
+				Name: "Unit Test Category",
+			}
+
+			updated, err := store.Update(objId, uc)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to update category by ID: %s.", test.Failed, testID, err)
+			}
+
+			if updated.Name != uc.Name {
+				t.Errorf("\t%s\tTest %d:\tShould be able to see updates to category.", test.Failed, testID)
+				t.Logf("\t\tTest %d:\tGot: %v", testID, updated.Name)
+				t.Logf("\t\tTest %d:\tExp: %v", testID, uc.Name)
+			} else {
+				t.Logf("\t%s\tTest %d:\tShould be able to see updates to category.", test.Success, testID)
+			}
+
+			if _, err := store.DeleteById(objId); err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to delete category : %s.", test.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to delete category.", test.Success, testID)
+
+			_, err = store.QueryById(objId)
+			if !errors.Is(err, validate.ErrNotFound) {
+				t.Fatalf("\t%s\tTest %d:\tShould NOT be able to retrieve category : %s.", test.Failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould NOT be able to retrieve user.", test.Success, testID)
+
 		}
 	}
 }
